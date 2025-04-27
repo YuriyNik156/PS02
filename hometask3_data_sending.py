f84efa1a322ff02df91c81f785d13498cc7de06f
YuriyNik156 <yuriy.nik156@gmail.com>
2025-04-27 12:26:24 +0300

    Create hometask3_data_sending.py

---
+++ hometask3_data_sending.py
@@ -0,0 +1,24 @@
+# Задание 3: Отправка данных
+# Используйте API, которое принимает POST-запросы для создания новых данных
+# (например, https://jsonplaceholder.typicode.com/posts).
+# Создайте словарь с данными для отправки (например, {'title': 'foo', 'body': 'bar', 'userId': 1}).
+# Отправьте POST-запрос с этими данными.
+# Распечатайте статус-код и содержимое ответа.
+
+import requests
+import pprint
+
+# Создание словаря с данными для отправки
+data = {
+    "title" : "foo",
+    "body" : "bar",
+    "userId" : 1
+}
+
+# Использование API
+response = requests.post("https://jsonplaceholder.typicode.com/posts", json=data)
+
+# Печать статус-кода и содержимое ответа
+print(response.status_code)
+
+pprint.pprint(response.json())
